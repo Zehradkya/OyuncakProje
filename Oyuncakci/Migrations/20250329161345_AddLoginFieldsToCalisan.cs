64bf396eb96931bc0669243491800fe172209d40
Zehra Demirkaya <zehrademirkayya@gmail.com>
2025-04-21 14:13:30 +0300

    Migrations
    Calisan modeline giriş alanları eklendi

---
+++ Oyuncakci/Migrations/20250329161345_AddLoginFieldsToCalisan.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Oyuncakci.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddLoginFieldsToCalisan : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "KullaniciAdi",
+                table: "Calisanlar",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "SifreHash",
+                table: "Calisanlar",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "KullaniciAdi",
+                table: "Calisanlar");
+
+            migrationBuilder.DropColumn(
+                name: "SifreHash",
+                table: "Calisanlar");
+        }
+    }
+}
